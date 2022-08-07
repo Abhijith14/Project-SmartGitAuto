43d1e72feaa242f4a86047e8601e32db62717b1a
Abhijith14 <abhijithukzm@gmail.com>
2022-08-07 14:59:36 +0530

    master Commit #2

--- cli_tool.py
+++ cli_tool.py
@@ -14,7 +14,7 @@ project_dir = os.listdir('.')
 
 def remove_symbols(data, elements=['\n', '\t']):
     for i in elements:
-        data = data.replace(i, "")
+        data = data.replace(i, " ")
 
     return data
 
@@ -24,17 +24,35 @@ def get_project_name(file_path='.git/config'):
     ret_data = []
     for data in pconfig:
         ret_data.append(remove_symbols(data))
-    purl = list(filter(lambda v: re.match('url', v), ret_data))
-    purl = purl[0].split("url = ")
+    purl = list(filter(lambda v: re.match(' url', v), ret_data))
+    purl = purl[0].split(" url = ")
 
     # extract name from url
     return purl[1].split('/')[-1]
 
 
-def get_project_version(file_path=''):
+def get_current_branch(file_path='.git/FETCH_HEAD'):
+    pconfig = open(file=file_path, mode='r')
+    ret_data = []
+    for data in pconfig:
+        ret_data.append(remove_symbols(data))
+    ret_data = ret_data[0].split("'")
+    return ret_data[1]
 
 
+def get_project_version(file_path='.git/logs/refs/heads/'):
+    curr_branch = get_current_branch()
+    pconfig = open(file=file_path+str(curr_branch))
+    ret_data = []
+    for data in pconfig:
+        # print(data)
+        ret_data.append(remove_symbols(data))
+    commit_c = 0
+    for i in ret_data:
+        commit_c = commit_c + i.count('commit:')
+
+    print(commit_c)
 
 if ".git" in project_dir: # Returns True if it is a git directory.
     print(get_project_name())
-
+    get_project_version()
